09d5999d59a4077967922aec5d6289a7f78b2d67
Anila Luta <118193110+AnilaLuta@users.noreply.github.com>
2023-05-13 21:11:03 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,1 +1,17 @@
+def sendMessage(message, socket):
+   socket.send(message)
 
+def connectToServer(IP, Port):
+   try:
+       clientSocket = soct.socket()
+       clientSocket.connect((IP, Port))
+
+   except ConnectionRefusedError:
+       print("Could not connect to the server")
+       clientSocket = None
+   return clientSocket
+
+message = ""
+serverIP = '127.0.0.1'
+serverPort = 8000
+socket = connectToServer(serverIP, serverPort)
